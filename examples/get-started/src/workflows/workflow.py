4954dc65e9e8ae81e1b303983f32e80468f7f891
leawn <leonrubner2809@gmail.com>
2024-10-25 19:14:19 +0200

    Update get-started example
    Former-commit-id: 1d6431470d15cc1fa5a094ba80afa457a22d0c2c

--- examples/get-started/src/workflows/workflow.py
+++ examples/get-started/src/workflows/workflow.py
@@ -1,14 +1,15 @@
 from datetime import timedelta
-from restack_ai.workflow import workflow
-from src.functions.function import welcome, goodbye, InputParams
+from restack_ai.workflow import workflow, workflow_import 
 
+with workflow_import():
+    from src.functions.function import welcome, goodbye, InputParams
 
 @workflow.defn(name="GreetingWorkflow")
 class GreetingWorkflow:
     @workflow.run
     async def run(self):
-        return await workflow.step(welcome, InputParams("world"), start_to_close_timeout=timedelta(seconds=10))
-    async def goodbye(self):
-        return await workflow.step(goodbye, InputParams("world"), start_to_close_timeout=timedelta(seconds=10))
+        welcome_result = await workflow.step(welcome, InputParams("world"), start_to_close_timeout=timedelta(seconds=10))
+        goodbye_result = await workflow.step(goodbye, InputParams("world"), start_to_close_timeout=timedelta(seconds=10))
+        return f"{welcome_result} {goodbye_result}"
 
 
