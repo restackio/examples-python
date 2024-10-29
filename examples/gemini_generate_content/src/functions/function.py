5d9bb8a7c4435a805d20f6536ce3f8d14f382368
leawn <leonrubner2809@gmail.com>
2024-10-29 07:06:18 +0100

    Working exmaple

---
+++ examples/gemini_generate_content/src/functions/function.py
@@ -0,0 +1,18 @@
+from restack_ai.function import function
+from pydantic import BaseModel
+from restack_integrations_gemini import gemini_generate_content, GeminiGenerateContentInput
+import os
+
+class FunctionInputParams(BaseModel):
+    user_content: str
+
+@function.defn(name="GeminiGenerateOpposite")
+async def gemini_generate_opposite(input: FunctionInputParams) -> str:
+    response = gemini_generate_content(
+        GeminiGenerateContentInput(
+            user_content=input.user_content,
+            model="gemini-1.5-flash",
+            api_key=os.environ.get("GEMINI_API_KEY"),
+        )
+    )
+    return response
