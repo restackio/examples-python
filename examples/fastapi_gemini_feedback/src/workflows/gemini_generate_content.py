c2a213f5f7b3ca9ec66a547c760aa602852b5a27
aboutphilippe <philippemarketing@gmail.com>
2024-11-03 11:11:18 -0800

    fix feedback

--- examples/fastapi_gemini_feedback/src/workflows/gemini_generate_content.py
+++ examples/fastapi_gemini_feedback/src/workflows/gemini_generate_content.py
@@ -13,7 +13,6 @@ class End:
 
 with workflow_import():
     from src.functions.function import gemini_generate, FunctionInputParams
-    from restack_google_gemini.task_queue import gemini_task_queue
 
 class WorkflowInputParams(BaseModel):
     user_content: str
@@ -23,10 +22,11 @@ class GeminiGenerateWorkflow:
     def __init__(self) -> None:
         self.end_workflow = False
         self.feedbacks = []
+        self.user_content = ""
     @workflow.event
     async def event_feedback(self, feedback: Feedback) -> Feedback:
         log.info(f"Received feedback: {feedback.feedback}")
-        return await workflow.step(gemini_generate, FunctionInputParams(user_content=f"{input.user_content}. Take into account all feedbacks: {feedback.feedback}"), start_to_close_timeout=timedelta(seconds=120))
+        return await workflow.step(gemini_generate, FunctionInputParams(user_content=f"{self.user_content}. Take into account all feedbacks: {feedback.feedback}"), start_to_close_timeout=timedelta(seconds=120))
     
     @workflow.event
     async def event_end(self, end: End) -> End:
@@ -35,7 +35,8 @@ class GeminiGenerateWorkflow:
         return end
     @workflow.run
     async def run(self, input: WorkflowInputParams):
-        await workflow.step(gemini_generate, FunctionInputParams(user_content=input.user_content), task_queue=gemini_task_queue, start_to_close_timeout=timedelta(seconds=120))
+        self.user_content = input.user_content
+        await workflow.step(gemini_generate, FunctionInputParams(user_content=input.user_content), start_to_close_timeout=timedelta(seconds=120))
         await workflow.condition(
             lambda: self.end_workflow
         )
