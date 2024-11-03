c697b645d04bb45eaccc1a7c588f903ececf8c90
aboutphilippe <philippemarketing@gmail.com>
2024-11-03 11:05:48 -0800

    fix fast api example

--- examples/fastapi_gemini_feedback/src/app.py
+++ examples/fastapi_gemini_feedback/src/app.py
@@ -22,34 +22,41 @@ class InputParams(BaseModel):
 
 @app.post("/api/schedule")
 async def schedule_workflow(data: InputParams):
+    
+    print(data)
+
     client = Restack()
 
     workflow_id = f"{int(time.time() * 1000)}-GeminiGenerateWorkflow"
-    runId = await client.schedule_workflow(
+    run_id = await client.schedule_workflow(
         workflow_name="GeminiGenerateWorkflow",
         workflow_id=workflow_id,
         input=data
     )
 
-    print(f"Scheduled workflow with ID: {workflow_id}")
+    print(f"Scheduled workflow with run_id: {run_id}")
 
-    result = await client.get_workflow_result(
-        workflow_id=workflow_id,
-        run_id=runId
-    )
-    return result
+    return {
+        "workflow_id": workflow_id,
+        "runId": run_id
+    }
 
 class FeedbackParams(BaseModel):
+    workflow_id: str
+    run_id: str
     feedback: str
 
 @app.post("/api/event/feedback")
 async def send_event_feedback(data: FeedbackParams):
     client = Restack()
 
+    print(data)
+
     await client.send_workflow_event(
-        workflow_name="GeminiGenerateWorkflow",
+        workflow_id=data.workflow_id,
+        run_id=data.run_id,
         event_name="feedback",
-        input=data
+        event_input={"feedback": data.feedback}
     )
     return
 
@@ -64,7 +71,7 @@ async def send_event_end():
     return
 
 def run_app():
-    uvicorn.run("src.app:app", host="0.0.0.0", port=5000, reload=True)
+    uvicorn.run("src.app:app", host="0.0.0.0", port=5001, reload=True)
 
 if __name__ == '__main__':
     run_app()
