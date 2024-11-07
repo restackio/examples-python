e954067bd3b75e29f9fa9ac273dc90cdfe67cd1e
Osman Hernandez <osman.sist@gmail.com>
2024-11-07 18:46:13 +0100

    Togetherai chat (#32)
    * use chat instead of just generate content
    
    * add message

--- examples/fastapi_togetherai_llama/src/functions/function.py
+++ examples/fastapi_togetherai_llama/src/functions/function.py
@@ -1,5 +1,6 @@
 from llama_index.llms.together import TogetherLLM
 from restack_ai.function import function, log, FunctionFailure
+from llama_index.core.llms import ChatMessage, MessageRole
 import os
 from pydantic import BaseModel
 from dotenv import load_dotenv
@@ -10,13 +11,20 @@ class FunctionInputParams(BaseModel):
     prompt: str
 
 @function.defn(name="llm_complete")
-async def llm_complete(input: FunctionInputParams) -> str:
+async def llm_complete(input: FunctionInputParams):
     try:
         llm = TogetherLLM(
             model="mistralai/Mixtral-8x7B-Instruct-v0.1", api_key=os.getenv("TOGETHER_API_KEY")
         )
-        resp = llm.complete(input.prompt)
-        return resp.text
+        messages = [
+            ChatMessage(
+                # This is a system prompt that is used to set the behavior of the LLM. You can update this llm_complete function to also accept a system prompt as an input parameter.
+                role=MessageRole.SYSTEM, content="You are a pirate with a colorful personality"
+            ),
+            ChatMessage(role=MessageRole.USER, content=input.prompt),
+        ]
+        resp = llm.chat(messages)
+        return resp.message.content
     except Exception as e:
         log.error(f"Error interacting with llm: {e}")
         raise FunctionFailure(f"Error interacting with llm: {e}", non_retryable=True)
