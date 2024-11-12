58ea58185f4a56e6cefbfe9fcb31756f57761fd1
martinibach <martin@restack.io>
2024-11-12 10:08:20 +0100

    services py

--- examples/fastapi_togetherai_llama/src/services.py
+++ examples/fastapi_togetherai_llama/src/services.py
@@ -2,11 +2,18 @@ import asyncio
 from src.client import client
 from src.functions.function import llm_complete
 from src.workflows.workflow import llm_complete_workflow
+from restack_ai.restack import ServiceOptions
 
 async def main():
-    await client.start_service(
-        workflows= [llm_complete_workflow],
-        functions= [llm_complete]
+    await asyncio.gather(
+        client.start_service(
+            workflows=[llm_complete_workflow],
+            functions=[llm_complete],
+            options=ServiceOptions(
+                rate_limit=1,
+                max_concurrent_function_runs=1
+            )
+        )
     )
 
 def run_services():
