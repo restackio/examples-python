c817b4100b601453d2f910e9223425422fbc6bbb
martinibach <martin@restack.io>
2024-11-22 12:20:34 +0100

    Refactor Dockerfile path for defense_quickstart_news_scraper_summarizer

--- examples/defense_quickstart_news_scraper_summarizer/restack_up.py
+++ examples/defense_quickstart_news_scraper_summarizer/restack_up.py
@@ -40,9 +40,9 @@ async def main():
 
     # Define the application configuration
     app = {
-        'name': 'defense_quickstart',
-        'dockerFilePath': '/examples/defense_quickstart/Dockerfile',
-        'dockerBuildContext': './examples/defense_quickstart/',
+        'name': 'defense_quickstart_news_scraper_summarizer',
+        'dockerFilePath': '/examples/defense_quickstart_news_scraper_summarizer/Dockerfile',
+        'dockerBuildContext': './examples/defense_quickstart_news_scraper_summarizer/',
         'environmentVariables': [
             {
                 'name': 'RESTACK_ENGINE_ID',
