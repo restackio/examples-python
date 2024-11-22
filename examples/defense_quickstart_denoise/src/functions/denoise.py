8cce5aa3164fa23dfbd9d7fbf9d8a56383e312ed
leawn <leonrubner2809@gmail.com>
2024-11-22 13:59:10 -0800

    denoise example

--- examples/defense_quickstart_denoise/src/functions/denoise.py
+++ examples/defense_quickstart_denoise/src/functions/denoise.py
@@ -1,28 +1,29 @@
 from restack_ai.function import function, log
 from dataclasses import dataclass
 import sieve
-import os
 
 @dataclass
 class FunctionInputParams:
     file_data: tuple[str, str]
 
 @function.defn()
-async def remove_noise(input: FunctionInputParams):
+async def denoise(input: FunctionInputParams):
     try:
-        log.info("remove_noise function started", input=input)
+        log.info("denoise function started", input=input)
 
-        file = sieve.File(path=input.file_data[0])
+        file_path, _ = input.file_data
+
+        file = sieve.File(path=file_path)
         backend = "aicoustics"
         task = "all"
         enhancement_steps = 64
 
         audio_enhance = sieve.function.get("sieve/audio-enhance")
-        output = audio_enhance.run(file, backend, task, enhancement_steps)
+        output = audio_enhance.run(file, backend, task, enhancement_steps).path
 
-        log.info("remove_noise function completed", output=output)
+        log.info("denoise function completed", output=output)
         return output        
 
     except Exception as e:
-        log.error("remove_noise function failed", error=e)
+        log.error("denoise function failed", error=e)
         raise e
