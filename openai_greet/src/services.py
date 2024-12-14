2efd5f30939b346b51fbb07e19096540926cb611
aboutphilippe <philippemarketing@gmail.com>
2024-12-14 12:29:46 -0800

    endpoints with openai greet example

--- openai_greet/src/services.py
+++ openai_greet/src/services.py
@@ -2,11 +2,16 @@ import asyncio
 from src.functions.function import openai_greet
 from src.client import client
 from src.workflows.openai_greet import OpenaiGreetWorkflow
+from restack_ai.restack import ServiceOptions
+
 async def main():
 
     await client.start_service(
         workflows= [OpenaiGreetWorkflow],
         functions= [openai_greet],
+        options=ServiceOptions(
+            endpoints= True
+        ),
     )
 
 def run_services():
