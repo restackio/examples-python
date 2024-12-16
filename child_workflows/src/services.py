32b543d0e7e11998b53b22fc3b16609bf1b73d22
martinibach <martin@restack.io>
2024-12-16 16:15:23 +0100

    child_workflows v0.49 1. Introduces API endpoints to the example 2. Adds Inputs for parent and child workflow 3. Add Bearer_token to env example if you want to enable authentication.

--- child_workflows/src/services.py
+++ child_workflows/src/services.py
@@ -1,18 +1,28 @@
 import asyncio
+import os
+from watchfiles import run_process
+from restack_ai.restack import ServiceOptions
 from src.functions.function import welcome
 from src.client import client
 from src.workflows.parent import ParentWorkflow
 from src.workflows.child import ChildWorkflow
 
 async def main():
-
     await client.start_service(
         workflows= [ParentWorkflow, ChildWorkflow],
-        functions= [welcome]
+        functions= [welcome],
+        options=ServiceOptions(
+            endpoints= True
+        ),
     )
 
 def run_services():
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Service interrupted by user. Exiting gracefully.")
 
-if __name__ == "__main__":
-    run_services()
+def watch_services():
+    watch_path = os.getcwd()
+    print(f"Watching {watch_path} and its subdirectories for changes...")
+    run_process(watch_path, recursive=True, target=run_services)
--- child_workflows/src/workflows/child.py
+++ child_workflows/src/workflows/child.py
@@ -1,14 +1,20 @@
 from datetime import timedelta
+from dataclasses import dataclass, field
 from restack_ai.workflow import workflow, import_functions, log
 with import_functions():
     from src.functions.function import welcome
+    
+
+@dataclass
+class ChildInput:
+    name: str = field(default="world")
 
 @workflow.defn()
 class ChildWorkflow:
     @workflow.run
-    async def run(self):
+    async def run(self, input: ChildInput):
         log.info("ChildWorkflow started")
-        result = await workflow.step(welcome, input="world", start_to_close_timeout=timedelta(seconds=120))
+        result = await workflow.step(welcome, input=input.name, start_to_close_timeout=timedelta(seconds=120))
         log.info("ChildWorkflow completed", result=result)
         return result
 
