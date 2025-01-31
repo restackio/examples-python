1258dbe0df9ca52c548f41ca5aeba7e775aa35c6
aboutphilippe <philippemarketing@gmail.com>
2025-01-31 12:44:17 +0100

    handle no tool call

--- agent_tool/src/agents/chat.py
+++ agent_tool/src/agents/chat.py
@@ -34,22 +34,26 @@ class AgentChat:
 
         log.info(f"tool_calls: {tool_calls}")
 
-        for tool_call in tool_calls:
-          log.info(f"tool_call: {tool_call}")
+        if tool_calls:
+            for tool_call in tool_calls:
+                log.info(f"tool_call: {tool_call}")
 
-          name = tool_call.function.name
-          args = json.loads(tool_call.function.arguments)
+                name = tool_call.function.name
+                args = json.loads(tool_call.function.arguments)
 
-          match name:
-            case SalesWorkflow.__name__:
-              log.info(f"calling {name} with args: {args}")
+                match name:
+                    case SalesWorkflow.__name__:
+                        log.info(f"calling {name} with args: {args}")
 
-              result = await workflow.child_execute(SalesWorkflow, workflow_id=tool_call.id, input=args)
-              self.messages.append(Message(role="tool", tool_call_id=tool_call.id, content=str(result)))
+                        result = await workflow.child_execute(SalesWorkflow, workflow_id=tool_call.id, input=args)
+                        self.messages.append(Message(role="tool", tool_call_id=tool_call.id, content=str(result)))
 
-              completion_with_tool_call = await workflow.step(llm_chat, LlmChatInput(messages=self.messages), start_to_close_timeout=timedelta(seconds=120))
-              self.messages.append(Message(role="assistant", content=completion_with_tool_call.choices[0].message.content or ""))
-              
+                        completion_with_tool_call = await workflow.step(llm_chat, LlmChatInput(messages=self.messages), start_to_close_timeout=timedelta(seconds=120))
+                        self.messages.append(Message(role="assistant", content=completion_with_tool_call.choices[0].message.content or ""))
+
+        else:
+            self.messages.append(Message(role="assistant", content=completion.choices[0].message.content or ""))
+
         return self.messages
     
     @workflow.event
