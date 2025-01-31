adac3c8b74b29d1bd190d2652d3d41c9383e238f
martinibach <martin@restack.io>
2025-01-31 13:23:10 -0800

    system promot and fix bug of duplicate message

--- agent_tool/src/agents/chat_tool_functions.py
+++ agent_tool/src/agents/chat_tool_functions.py
@@ -26,14 +26,13 @@ class AgentChatToolFunctions:
     @workflow.event
     async def message(self, message: MessageEvent) -> List[Message]:
         log.info(f"Received message: {message.content}")
-
         
         tools = [pydantic_function_tool(
             model=LookupSalesInput,
             name=lookupSales.__name__,
             description="Lookup sales for a given category"
         ),
-        # Step 3 Add your new function to the tools list
+        # Step 3 Add your new function to the tools list and adjust the system prompt
         
         # pydantic_function_tool(
         #     model=FunctionInput,
@@ -41,9 +40,12 @@ class AgentChatToolFunctions:
         #     description="A function to talk to an ERP to get the latest sales data"
         # )
         ]
+        
+        # Change the system prompt to the agent
+        system_content = "You are a helpful assistant that can help with sales data."
 
         self.messages.append(Message(role="user", content=message.content or ""))
-        completion = await workflow.step(llm_chat, LlmChatInput(messages=self.messages, tools=tools), start_to_close_timeout=timedelta(seconds=120))
+        completion = await workflow.step(llm_chat, LlmChatInput(messages=self.messages, tools=tools, system_content=system_content), start_to_close_timeout=timedelta(seconds=120))
 
         log.info(f"completion: {completion}")
         
@@ -67,7 +69,7 @@ class AgentChatToolFunctions:
                         result = await workflow.step(lookupSales, input=LookupSalesInput(category=args.category), start_to_close_timeout=timedelta(seconds=120))
                         self.messages.append(Message(role="tool", tool_call_id=tool_call.id, content=str(result)))
 
-                        completion_with_tool_call = await workflow.step(llm_chat, LlmChatInput(messages=self.messages), start_to_close_timeout=timedelta(seconds=120))
+                        completion_with_tool_call = await workflow.step(llm_chat, LlmChatInput(messages=self.messages, system_content=system_content), start_to_close_timeout=timedelta(seconds=120))
                         self.messages.append(Message(role="assistant", content=completion_with_tool_call.choices[0].message.content or ""))
                     
                     ## Step 4: Add your new function to the match case and append the result to the messages
@@ -84,7 +86,7 @@ class AgentChatToolFunctions:
                     
 
         else:
-            self.messages.append(Message(role="assistant", content=completion.choices[0].message.content or ""))
+            pass
 
         return self.messages
     
