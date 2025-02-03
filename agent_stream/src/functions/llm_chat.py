b4d3cb85fe6fef4d79102da6746bd14d30d1d463
aboutphilippe <philippemarketing@gmail.com>
2025-02-03 13:48:10 +0100

    agent in restack ai package

--- agent_stream/src/functions/llm_chat.py
+++ agent_stream/src/functions/llm_chat.py
@@ -1,11 +1,10 @@
-from ..client import api_address
-from restack_ai.function import function, log, function_info, heartbeat
+from restack_ai.function import function, log, stream_to_websocket
 from openai import OpenAI
-from openai.types.chat.chat_completion import ChatCompletion
+from openai.resources.chat.completions import Stream, ChatCompletionChunk
 import os
 from pydantic import BaseModel
 from typing import Literal, Optional, List
-import websockets
+from ..client import api_address
 
 class Message(BaseModel):
     role: Literal["system", "user", "assistant"]
@@ -14,69 +13,32 @@ class Message(BaseModel):
 class LlmChatInput(BaseModel):
     system_content: Optional[str] = None
     model: Optional[str] = None
-    messages: Optional[List[Message]] = None
+    messages: Optional[List[Message]] = []
 
 @function.defn()
 async def llm_chat(input: LlmChatInput) -> str:
     try:
-        workflow_id = function_info().workflow_id
-        run_id = function_info().workflow_run_id
-
-        log.info("llm_chat function started", input=input)
-        client = OpenAI(base_url="https://ai.restack.io", api_key=os.environ.get("RESTACK_API_KEY"))
+        client = OpenAI(
+            base_url="https://ai.restack.io",
+            api_key=os.environ.get("RESTACK_API_KEY")
+        )
 
         if input.system_content:
-            input.messages.append({"role": "system", "content": input.system_content})
+            input.messages.insert(Message(role="system", content=input.system_content))
 
-        # Initialize a list to collect the messages
-        collected_messages = []
+        # Convert Message objects to dictionaries
+        messages_dicts = [message.model_dump() for message in input.messages]
+        # Get the streamed response from OpenAI API
+        response: Stream[ChatCompletionChunk] = client.chat.completions.create(
+            model=input.model or "restack-c1",
+            messages=messages_dicts,
+            stream=True,
+        )
 
-        # Create a WebSocket connection
-        websocket_url = f"ws://{api_address}/ws?workflowId={workflow_id}&runId={run_id}"
-        async with websockets.connect(websocket_url) as websocket:
-            try:
-                
-                info = {
-                    "activityId": function_info().activity_id,
-                    "workflowId": function_info().workflow_id,
-                    "runId": function_info().workflow_run_id,
-                    "activityType": function_info().activity_type,
-                    "taskQueue": function_info().task_queue,
-                }
-                # Send initial message
-                await websocket.send(str(info))
-                heartbeat(info)
-
-                # Get the streamed response from OpenAI API
-                response:ChatCompletion = client.chat.completions.create(
-                    model=input.model or "restack-c1",
-                    messages=input.messages,
-                    stream=True,
-                )
-
-                # Iterate over the streamed response and send each chunk to WebSocket
-                for chunk in response:
-                    content = chunk.choices[0].delta.content
-                    collected_messages.append(content)
-                    if chunk.choices[0].delta:
-                      chunkInfo = {
-                          **info,
-                          "chunk": chunk.__dict__
-                      }
-                      log.info("sending content to websocket", chunkInfo=chunkInfo)
-                      heartbeat(chunkInfo)
-                      await websocket.send(str(chunkInfo))
-
-                collected_messages = [m for m in collected_messages if m is not None]
-                final_response = ''.join(collected_messages)
-
-                log.info("llm_chat function completed", final_response=final_response)
-                return str(final_response)  # Modify the return type if necessary
-
-            finally:
-                # Ensure the WebSocket connection is closed
-                await websocket.close()
+        # Use Restack API websocket to stream the response
+        final_response = await stream_to_websocket("localhost:6666", response)
+        return final_response
 
     except Exception as e:
-        log.error("llm_chat function failed", error=e)
+        log.error("llm_chat function failed", error=str(e))
         raise e
